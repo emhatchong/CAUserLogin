4d7da76e9cab1539ba09a06b32f8ed3a650678bf
emhatchong <emily.tchong@mail.utoronto.ca>
2024-10-21 19:42:40 -0400

    Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,5 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the currentuser's name.
+     * @param name the name of the current user.
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return the current username as a string.
+     */
+    String getCurrentUser();
 }
